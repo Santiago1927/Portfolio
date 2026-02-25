1abe7bb9f9d3cf8e465d0d82e0c0fa9062a54a51
David Santiago Andrade <davidandradesantacruz9.3@gmail.com>
2026-02-25 15:08:17 -0500

    Ajustes build

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -179,7 +179,7 @@ export default function Certificates() {
       }, TRANSITION_MS);
       return () => clearTimeout(timer);
     }
-  }, [displayIndex]);
+  }, [displayIndex, N]);
 
   useEffect(() => {
     if (!animated) {
